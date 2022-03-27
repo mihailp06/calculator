9282a093b07077554af97a7b6120ee036d0ca9bc
mihailp06 <mihailpetkov120@gmail.com>
2022-03-27 09:35:55 +0300

    Add operate function

--- script.js
+++ script.js
@@ -13,3 +13,24 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+function operate(operator, a, b) {
+    let result;
+    switch (operator) {
+        case '+':
+            result = add(a, b);
+            break;
+        case '-':
+            result = subtract(a, b);
+            break;
+        case '*':
+            result = multiply(a, b);
+            break;
+        case '/':
+            result = divide(a, b);
+            break;
+        default:
+            result = 'ERROR';
+    }
+    return result;
+}
