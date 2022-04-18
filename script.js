89292a67d608f97322f1c0021f8c0a41afd311e2
mihailp06 <mihailpetkov120@gmail.com>
2022-04-18 12:39:41 +0300

    Add functions to buttons

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
+let previousOperand = '';
+let currentOperand = '';
+let operator = '';
+
+const previousOperandDiv = document.querySelector('[data-previous-operand]');
+const currentOperandDiv = document.querySelector('[data-current-operand]');
+const numberButtons = document.querySelectorAll('[data-number]');
+const operatorButtons = document.querySelectorAll('[data-operator]');
+const clearBtn = document.querySelector('[data-clear]');
+const equalsBtn = document.querySelector('[data-equals]');
+
+
 function add(a, b) {
-    return a + b;
+    return +a + +b;
 }
 
 function subtract(a, b) {
-    return a - b;
+    return +a - +b;
 }
 
 function multiply(a, b) {
-    return a * b;
+    return +a * +b;
 }
 
 function divide(a, b) {
-    return a / b;
+    return +a / +b;
 }
 
 function operate(operator, a, b) {
@@ -34,4 +46,47 @@ function operate(operator, a, b) {
     }
     return result;
 }
+
+function clear() {
+    previousOperand = '';
+    currentOperand = '';
+}
+
+function appendNumber(num) {
+    if (num === '.' && currentOperand.includes('.')) return;
+    currentOperand += num;
+}
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        appendNumber(button.textContent);
+        currentOperandDiv.textContent = currentOperand;
+    });
+});
+
+operatorButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        operator = button.textContent;
+        previousOperand = currentOperand;
+        previousOperandDiv.textContent = `${previousOperand} ${operator}`;
+        currentOperand = '';
+        currentOperandDiv.textContent = currentOperand;
+    });
+});
+
+equalsBtn.addEventListener('click', () => {
+    if (previousOperand !== '') {
+        currentOperand = operate(operator, previousOperand, currentOperand).toString();
+        previousOperandDiv.textContent = '';
+        currentOperandDiv.textContent = currentOperand;
+    }
+});
+
+clearBtn.addEventListener('click', () => {
+    clear();
+    currentOperandDiv.textContent = currentOperand;
+    previousOperandDiv.textContent = previousOperand;
+});
+
+
 
